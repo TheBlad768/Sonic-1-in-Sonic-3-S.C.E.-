207d49e4ddb7414191e7f99efe8f9c24201eef22
TheBlad768 <master.metalsonic2014@gmail.com>
2025-11-10 23:08:01 +0400

    Comments update

--- Engine/Core/Interrupt Handler.asm
+++ Engine/Core/Interrupt Handler.asm
@@ -58,7 +58,7 @@ VInt_Done:
 ; =============== S U B R O U T I N E =======================================
 
 VInt_Lag:
-		addq.w	#4,sp
+		addq.w	#4,sp								; do not execute "VInt_Done" twice
 
 VInt_Lag_Main:
 		addq.w	#1,(Lag_frame_count).w
@@ -72,6 +72,8 @@ VInt_Lag_Main:
 		bne.s	VInt_Music							; if not, return from V-int
 
 VInt_Lag_Level:
+
+		; check water
 		tst.b	(Water_flag).w
 		beq.s	VInt_Lag_NoWater
 
