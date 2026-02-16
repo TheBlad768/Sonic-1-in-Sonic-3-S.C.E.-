dbdacda0bdd70b890483f252a036a586c9210978
TheBlad768 <master.metalsonic2014@gmail.com>
2026-02-16 20:06:33 +0400

    Object constants update (TEST)

--- Objects/Bosses/GHZ3/Boss.asm
+++ Objects/Bosses/GHZ3/Boss.asm
@@ -262,8 +262,15 @@ BossBall_Defeated:
 ; ---------------------------------------------------------------------------
 
 ; dynamic object variables
-bossball_crane.moving			= objoff_39	; .b
-bossball_crane.speed			= objoff_3A	; .w
+
+	dsset aniraw_ptr								; pretend we're in the RAM
+
+bossball_crane			= *
+
+.speed				ds.w 1							; (2 bytes)
+.moving				ds.b 1							; (1 byte)
+
+	dsreset										; stop pretending and reset the program counter
 
 ; =============== S U B R O U T I N E =======================================
 
