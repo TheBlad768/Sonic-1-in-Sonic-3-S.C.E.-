30465e0550fe00cf1ec9b2d9ac131b5ec959ee89
TheBlad768 <master.metalsonic2014@gmail.com>
2026-02-26 04:25:58 +0400

    titlecardMapLetters macro
    Now it's easier to name zones for Title Card!

---
+++ Objects/Main/Title Card/Text Data/Map - Text Data.asm
@@ -0,0 +1,14 @@
+; ===========================================================================
+; Text Mappings (Title Card)
+; ===========================================================================
+
+; generate title card letter mappings
+Map_TitleCard_ZONE:	titlecardMapLetters FALSE, ((screen_width/2)-124), "ZONE"
+Map_TitleCard_GHZ:	titlecardMapLetters TRUE, ((screen_width/2)-32), "GREEN HILL"
+Map_TitleCard_MZ:	titlecardMapLetters TRUE, ((screen_width/2)-32), "MARBLE"
+Map_TitleCard_SYZ:	titlecardMapLetters TRUE, ((screen_width/2)-32), "SPRING YARD"
+Map_TitleCard_LZ:	titlecardMapLetters TRUE, ((screen_width/2)-32), "LABYRINTH"
+Map_TitleCard_SLZ:	titlecardMapLetters TRUE, ((screen_width/2)-32), "STAR LIGHT"
+Map_TitleCard_SBZ:	titlecardMapLetters TRUE, ((screen_width/2)-32), "SCRAP BRAIN"
+Map_TitleCard_FZ:	titlecardMapLetters TRUE, ((screen_width/2)-32), "FINAL"
+	even
