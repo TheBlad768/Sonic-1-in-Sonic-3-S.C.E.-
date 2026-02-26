eb4a986367fb1d488e7c26efb73291185265cb88
TheBlad768 <master.metalsonic2014@gmail.com>
2026-02-27 01:54:22 +0400

    titlecardMapLetters macro fix
    0 = O

--- Objects/Main/Title Card/Text Data/Map - Text Data.asm
+++ Objects/Main/Title Card/Text Data/Map - Text Data.asm
@@ -3,7 +3,7 @@
 ; ===========================================================================
 
 ; generate title card letter mappings
-Map_TitleCard_ZONE:	titlecardMapLetters FALSE, ((screen_width/2)-124), TitleCardName_ZONE
+Map_TitleCard_ZONE:	titlecardMapLetters FALSE, -124, TitleCardName_ZONE
 Map_TitleCard_GHZ:	titlecardMapLetters TRUE, , TitleCardName_GHZ
 Map_TitleCard_MZ:	titlecardMapLetters TRUE, , TitleCardName_MZ
 Map_TitleCard_SYZ:	titlecardMapLetters TRUE, , TitleCardName_SYZ
