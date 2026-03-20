a5e7cc1fe8222ef89c5af027a2c9b8d3809ee26b
TheBlad768 <master.metalsonic2014@gmail.com>
2026-03-20 04:56:25 +0400

    Sinking Mud update
    - Title Screen fix

--- Objects/Main/Sinking Mud/Sinking Mud.asm
+++ Objects/Main/Sinking Mud/Sinking Mud.asm
@@ -4,104 +4,91 @@
 
 ; dynamic object variables
 
+	dsset aniraw_ptr								; pretend we're in the RAM
+
+; players
+sinkingmud.p1_depth			ds.b 1						; Sonic's depth of the mud (1 byte)
+sinkingmud.p2_depth			ds.b 1						; Tails's depth of the mud (1 byte)
+
+	dsreset										; stop pretending and reset the program counter
+
 ; =============== S U B R O U T I N E =======================================
 
 Obj_SinkingMud:
 
-		; set
+		; calc
 		move.b	subtype(a0),d0
 		lsl.b	#3,d0								; multiply by 8
 		move.b	d0,width_pixels(a0)
-		move.w	y_pos(a0),objoff_30(a0)
+
+		; set
 		moveq	#$30,d0
-		move.b	d0,objoff_38(a0)
-		move.b	d0,objoff_3A(a0)
+		move.b	d0,sinkingmud.p1_depth(a0)
+		move.b	d0,sinkingmud.p2_depth(a0)
 		bset	#status.npc.no_balancing,status(a0)				; disable player's balance animation
 		move.l	#.main,address(a0)
 
 .main
+
+		; check debug
 		tst.w	(Debug_placement_mode).w					; is debug mode on?
-		bne.w	loc_32B62							; if yes, branch
+		bne.s	.chkdel								; if yes, branch
 
 		; check p1
-		lea	(Player_1).w,a1
-		btst	#p1_standing_bit,status(a0)
-		bne.s	loc_32AEA
-		cmpi.b	#$30,objoff_38(a0)
-		bhs.s	loc_32ACE
-		addq.b	#2,objoff_38(a0)
-
-loc_32ACE:
-		btst	#status.player.on_object,status(a1)
-		beq.s	loc_32AF4
-		movea.w	interact(a1),a2
-		cmpi.l	#Obj_SinkingMud.main,address(a2)
-		bne.s	loc_32AF4
-		move.b	objoff_38(a2),objoff_38(a0)
-		bra.s	loc_32AF4
-; ---------------------------------------------------------------------------
-
-loc_32AEA:
-		tst.b	objoff_38(a0)
-		beq.s	loc_32B68
-		subq.b	#1,objoff_38(a0)
-
-loc_32AF4:
-		moveq	#0,d1
-		move.b	width_pixels(a0),d1
-		moveq	#0,d3
-		move.b	objoff_38(a0),d3
+		lea	(Player_1).w,a1							; a1=character
+		lea	sinkingmud.p1_depth(a0),a2
 		moveq	#p1_standing_bit,d6
-		move.w	x_pos(a0),d4
-		jsr	(SolidObjectTop.check).w
+		bsr.s	.process
 
 		; check p2
-		lea	(Player_2).w,a1
-		btst	#p2_standing_bit,status(a0)
-		bne.s	loc_32B40
-		cmpi.b	#$30,objoff_3A(a0)
-		bhs.s	loc_32B24
-		addq.b	#2,objoff_3A(a0)
-
-loc_32B24:
-		btst	#status.player.on_object,status(a1)
-		beq.s	loc_32B4A
-		movea.w	interact(a1),a2
-		cmpi.l	#Obj_SinkingMud.main,address(a2)
-		bne.s	loc_32B4A
-		move.b	objoff_3A(a2),objoff_3A(a0)
-		bra.s	loc_32B4A
-; ---------------------------------------------------------------------------
-
-loc_32B40:
-		tst.b	objoff_3A(a0)
-		beq.s	loc_32B70
-		subq.b	#1,objoff_3A(a0)
-
-loc_32B4A:
-		moveq	#0,d1
-		move.b	width_pixels(a0),d1
-		moveq	#0,d3
-		move.b	objoff_3A(a0),d3
+		lea	(Player_2).w,a1							; a1=character
+		tst.l	address(a1)							; is the player RAM empty?
+		beq.s	.chkdel								; if yes, branch
+		lea	sinkingmud.p2_depth(a0),a2
 		moveq	#p2_standing_bit,d6
-		move.w	x_pos(a0),d4
-		jsr	(SolidObjectTop.check).w
+		bsr.s	.process
 
-loc_32B62:
+.chkdel
 		jmp	(Delete_Sprite_If_Not_In_Range).w
 ; ---------------------------------------------------------------------------
 
-loc_32B68:
-		move.b	#$30,objoff_38(a0)
-		bra.s	loc_32B76
+.process
+		btst	d6,status(a0)							; is the player standing on the current object?
+		bne.s	.sinking							; if yes, branch
+
+		; smoothly restore the depth
+		cmpi.b	#$30,(a2)
+		bhs.s	.check
+		addq.b	#2,(a2)
+
+.check
+		btst	#status.player.on_object,status(a1)				; is player standing on an object?
+		beq.s	.solid								; if not, branch
+		movea.w	interact(a1),a3
+		cmpi.l	#Obj_SinkingMud.main,address(a3)
+		bne.s	.solid
+		move.w	a2,d0
+		sub.w	a0,d0
+		move.b	(a3,d0.w),(a2)
+		bra.s	.solid
 ; ---------------------------------------------------------------------------
 
-loc_32B70:
-		move.b	#$30,objoff_3A(a0)
+.sinking
+		tst.b	(a2)
+		beq.s	.kill_player
+		subq.b	#1,(a2)
 
-loc_32B76:
-		not.b	d1
-		and.b	d1,status(a0)
+.solid
+		moveq	#0,d1
+		move.b	width_pixels(a0),d1
+		moveq	#0,d3
+		move.b	(a2),d3								; height_pixels
+		move.w	x_pos(a0),d4
+		jmp	(SolidObjectTop.check).w
+; ---------------------------------------------------------------------------
+
+.kill_player
+		move.b	#$30,(a2)							; reset depth of the mud
 
 		; kill player
 		move.w	a0,-(sp)
