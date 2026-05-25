dd600c85bf39c458c9874aa01c44d7e85e3ef950
TheBlad768 <master.metalsonic2014@gmail.com>
2026-05-25 23:05:53 +0400

    palptr macro update

--- Data/Palette Pointers.asm
+++ Data/Palette Pointers.asm
@@ -2,32 +2,34 @@
 ; Palette pointers
 ; ===========================================================================
 
-PalPointers:				; palette address, RAM address
+PalPointers:							; palette address, RAM address
 
-; Main
-PalPtr_Sonic:				palptr Pal_Sonic, 0				; 0 - Sonic
-PalPtr_WaterSonic:			palptr Pal_WaterSonic, 0			; 1 - Water Sonic
-PalPtr_WaterSonicLZ:			palptr Pal_WaterSonicLZ, 0			; 2 - Water Sonic LZ
-PalPtr_WaterSonicSBZ:			palptr Pal_WaterSonicSBZ, 0			; 3 - Water Sonic SBZ
-PalPtr_Knuckles:			palptr Pal_Knuckles, 0				; 4 - Knuckles
-PalPtr_WaterKnuckles:			palptr Pal_WaterKnuckles, 0			; 5 - Water Knuckles
-PalPtr_WaterKnucklesLZ:			palptr Pal_WaterKnucklesLZ, 0			; 6 - Water Knuckles LZ
-PalPtr_WaterKnucklesSBZ:		palptr Pal_WaterKnucklesSBZ, 0			; 7 - Water Knuckles SBZ
+		; main
+		palptr Pal_Sonic, 0				; 0 - Sonic
+		palptr Pal_WaterSonic, 0			; 1 - Water Sonic
+		palptr Pal_WaterSonicLZ, 0			; 2 - Water Sonic LZ
+		palptr Pal_WaterSonicSBZ, 0			; 3 - Water Sonic SBZ
+		palptr Pal_Knuckles, 0				; 4 - Knuckles
+		palptr Pal_WaterKnuckles, 0			; 5 - Water Knuckles
+		palptr Pal_WaterKnucklesLZ, 0			; 6 - Water Knuckles LZ
+		palptr Pal_WaterKnucklesSBZ, 0			; 7 - Water Knuckles SBZ
 
-; Levels
-PalPtr_GHZ:				palptr Pal_GHZ, 1				; 8 - GHZ
-PalPtr_WaterGHZ:			palptr Pal_WaterGHZ, 1				; 9 - Water GHZ
-PalPtr_MZ:				palptr Pal_MZ, 1				; A - GHZ
-PalPtr_WaterMZ:				palptr Pal_WaterMZ, 1				; B - Water GHZ
-PalPtr_SYZ:				palptr Pal_SYZ, 1				; C - SYZ
-PalPtr_WaterSYZ:			palptr Pal_WaterSYZ, 1				; D - Water SYZ
-PalPtr_LZ:				palptr Pal_LZ, 1				; E - LZ
-PalPtr_WaterLZ:				palptr Pal_WaterLZ, 1				; F - Water LZ
-PalPtr_SLZ:				palptr Pal_SLZ, 1				; 10 - SLZ
-PalPtr_WaterSLZ:			palptr Pal_WaterSLZ, 1				; 11 - Water SLZ
-PalPtr_SBZ1:				palptr Pal_SBZ1, 1				; 12 - SBZ1
-PalPtr_SBZ2:				palptr Pal_SBZ2, 1				; 13 - SBZ2
-PalPtr_SBZ3:				palptr Pal_SBZ3, 1				; 14 - SBZ3
-PalPtr_WaterSBZ3:			palptr Pal_WaterSBZ3, 1				; 15 - Water SBZ3
-PalPtr_Title:				palptr Pal_Title, 0				; 16 - Title
-PalPtr_Ending:				palptr Pal_Ending, 1				; 17 - Ending
+		; levels
+		palptr Pal_GHZ, 1				; 8 - GHZ
+		palptr Pal_WaterGHZ, 1				; 9 - Water GHZ
+		palptr Pal_MZ, 1				; A - GHZ
+		palptr Pal_WaterMZ, 1				; B - Water GHZ
+		palptr Pal_SYZ, 1				; C - SYZ
+		palptr Pal_WaterSYZ, 1				; D - Water SYZ
+		palptr Pal_LZ, 1				; E - LZ
+		palptr Pal_WaterLZ, 1				; F - Water LZ
+		palptr Pal_SLZ, 1				; 10 - SLZ
+		palptr Pal_WaterSLZ, 1				; 11 - Water SLZ
+		palptr Pal_SBZ1, 1				; 12 - SBZ1
+		palptr Pal_SBZ2, 1				; 13 - SBZ2
+		palptr Pal_SBZ3, 1				; 14 - SBZ3
+		palptr Pal_WaterSBZ3, 1				; 15 - Water SBZ3
+
+		; misc
+		palptr Pal_Title, 0				; 16 - Title
+		palptr Pal_Ending, 1				; 17 - Ending
